cff9b56850bd42bd23be65512c26b24556fc402e
PC <seenone@gmail.com>
2021-01-25 12:30:47 +0300

    scripts

---
+++ ML_RANS/Data_generation/change_speed.py
@@ -0,0 +1,26 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Changes velocity(U) in inlet condition.')
+parser.add_argument('U', metavar='u', type=float, help='new value for the U')
+
+
+args = parser.parse_args()
+u = args.U
+ 
+f = open("U","r")
+lines = f.readlines()
+lines[25] =f"        value           uniform ({args.U} 0 0);\n"
+f = open("U","w")
+
+f.writelines(lines)
+f.close()
+
+
+    print(f"changing velocity to {i}")
+    f = open("U","r")
+    lines = f.readlines()
+    lines[25] =f"        value           uniform ({i} 0 0);\n"
+    f = open("U","w")
+    f.writelines(lines)
+    f.close()
